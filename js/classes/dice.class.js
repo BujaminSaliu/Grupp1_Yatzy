02ad3e2208280099d83f930ac35e6440508774c8
Christoffer Flystam <christoffer.flystam@gmail.com>
2017-03-23 16:24:17 +0100

    Added write method
    Still needs work

--- js/classes/dice.class.js
+++ js/classes/dice.class.js
@@ -10,4 +10,30 @@ class Dice{
 		return Math.floor(Math.random() * 6) + 1;
 	}
 	
+	//Denna är inte klar
+	writeDiceToDOM(){
+
+	switch(this.currentValue){
+		case 1:
+			document.write("<IMG data-id=" + this.diceNumber + " SRC='1.png'>");
+			break;
+		case 2:
+			document.write("<IMG data-id=" + this.diceNumber + " SRC='2.png'>");
+			break;
+		case 3:
+			document.write("<IMG data-id=" + this.diceNumber + " SRC='3.png'>");
+			break;
+		case 4:
+			document.write("<IMG data-id=" + this.diceNumber + " SRC='4.png'>");
+			break;
+		case 5:
+			document.write("<IMG data-id=" + this.diceNumber + " SRC='5.png'>");
+			break;
+		case 6:
+			document.write("<IMG data-id=" + this.diceNumber + " SRC='6.png'>");
+			break;
+
+	}
+
+}
 }
