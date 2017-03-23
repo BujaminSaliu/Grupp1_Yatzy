5da5942d809c4cd2b0abec5ac0c4ee5961fe31a2
Christoffer Flystam <christoffer.flystam@gmail.com>
2017-03-23 16:10:11 +0100

    Cleaned up class
    Only whats relevant to the task is now in Dice class. Grouped out dice-pictures in folder img

--- js/main.js
+++ js/main.js
@@ -1,23 +1,10 @@
 let dice1 = new Dice(1);
 
-dice1.Roll(true);
 let dice2 = new Dice(2);
-dice2.Roll(true);
 
 let dice3 = new Dice(3);
-dice3.Roll(true);
 
 let dice4 = new Dice(4);
-dice4.Roll(true);
 
 let dice5 = new Dice(5);
-dice5.Roll(true);
 
-function Rolling(){
-	dice1.Roll(false);
-	
-	dice2.Roll(false);
-	dice3.Roll(false);
-	dice4.Roll(false);
-	dice5.Roll(false);
-}
