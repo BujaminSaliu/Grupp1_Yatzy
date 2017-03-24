1c5aa4dfa8b4aeea1d10da189c05cd75862a0fa2
Christoffer Flystam <christoffer.flystam@gmail.com>
2017-03-24 13:08:10 +0100

    Rolling of dice completed witch locking

--- js/main.js
+++ js/main.js
@@ -17,15 +17,33 @@ $( document ).ready(function() {
 
 function testRenderingOfDices() {
 	for(dice of dices) {
-		console.log(dice);
 		dice.writeDiceToDOM();
 	}
 }
 
 function testRoll() {
+	lockCheckedDices();
+
 	for(dice of dices) {
 		dice.clearDicesInDOM();
 		dice.roll();
 		dice.writeDiceToDOM();
 	}
+}
+
+function lockCheckedDices() {
+	var checkBoxes = $('.check-container').children();
+
+	for(checkBox of checkBoxes) {
+		if($('#' + checkBox.id).is(":checked")) {
+			var idToLock = parseCheckBoxIdToIndexOfDice(checkBox.id);
+			dices[idToLock].lockDice();
+		}
+	}
+}
+
+function parseCheckBoxIdToIndexOfDice(checkBoxId) {
+	var idSplits = checkBoxId.split('-');
+	var indexOfDice = parseInt(idSplits[1]);
+	return indexOfDice;
 }
