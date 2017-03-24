b4735bb3a7bb0844e1dabb7d6e5bc3f19aa1055d
Christoffer Flystam <christoffer.flystam@gmail.com>
2017-03-24 10:16:53 +0100

    Made dice appear in center of game-board

--- js/classes/dice.class.js
+++ js/classes/dice.class.js
@@ -17,29 +17,29 @@ class Dice{
 		console.log(this.currentValue);
 		switch(this.currentValue){
 			case 1:
-				$('body').append("<IMG data-id=" + this.diceNumber + " SRC='/img/1.png'>");
+				$('#dice-container-' + `${this.diceNumber}`).append("<IMG data-id=" + this.diceNumber + " SRC='/img/1.png'>");
 				break;
 			case 2:
-				$('body').append("<IMG data-id=" + this.diceNumber + " SRC='/img/2.png'>");
+				$('#dice-container-' + `${this.diceNumber}`).append("<IMG data-id=" + this.diceNumber + " SRC='/img/2.png'>");
 				break;
 			case 3:
-				$('body').append("<IMG data-id=" + this.diceNumber + " SRC='/img/3.png'>");
+				$('#dice-container-' + `${this.diceNumber}`).append("<IMG data-id=" + this.diceNumber + " SRC='/img/3.png'>");
 				break;
 			case 4:
-				$('body').append("<IMG data-id=" + this.diceNumber + " SRC='/img/4.png'>");
+				$('#dice-container-' + `${this.diceNumber}`).append("<IMG data-id=" + this.diceNumber + " SRC='/img/4.png'>");
 				break;
 			case 5:
-				$('body').append("<IMG data-id=" + this.diceNumber + " SRC='/img/5.png'>");
+				$('#dice-container-' + `${this.diceNumber}`).append("<IMG data-id=" + this.diceNumber + " SRC='/img/5.png'>");
 				break;
 			case 6:
-				$('body').append("<IMG data-id=" + this.diceNumber + " SRC='/img/6.png'>");
+				$('#dice-container-' + `${this.diceNumber}`).append("<IMG data-id=" + this.diceNumber + " SRC='/img/6.png'>");
 				break;
 		}
 
 	}
 
 	clearDicesInDOM(){
-		$('body').empty();
+		$('#dices-in-play').empty();
 	}
 
 }
