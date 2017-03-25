f694aee789e0c490d954920bbafa078e267e1585
Alexander Aschan <alexander.aschan@live.se>
2017-03-25 14:00:43 +0100

    added function to calculate full house

--- js/classes/scoreboard.class.js
+++ js/classes/scoreboard.class.js
@@ -6,4 +6,36 @@ class ScoreBoard{
 		this.bonus = 50;
 		this.totalScore = 0;
 	}
+
+
+	checkFullHouse(listOfDice){
+		threeOfAKind = false;
+		pair = false;
+		returnvalue = 0;
+		for(i = 1; i < 7; i++){
+			counter = 0;
+			for(j = 0; j < listOfDice.length; j++){
+				if(listOfDice[j].currentValue === i){
+					counter++;
+				}
+			}
+
+			if(counter === 2){
+				pair = true;
+			}
+
+			if(counter === 3){
+				threeOfAKind = true;
+			}
+		}
+
+		if(threeOfAKind === true && pair === true){
+			returnvalue = sumDices(listOfDice);
+		}
+
+		return returnvalue;
+
+	}
+
+
 }
