63e6ea0546a744eb83a06a1482014d94d921336d
Bekir Halvadzic <bekir_h@hotmail.com>
2017-04-05 09:52:42 +0200

    Changes the 4 available letters to the right names in scoreboard
    it also cuts the names to 4 lettters

--- js/classes/game.class.js
+++ js/classes/game.class.js
@@ -15,6 +15,12 @@ class Game{
 		for(let i = 0; i < this.scoreBoards.length; i++){
 			$('#'+ i + '-sum').append(this.scoreBoards[i].bonusScore);
 			$('#'+ i + '-totalSum').append(this.scoreBoards[i].bonusScore);
+			//Saves, cuts the playernames to 4 letters and adds it to the right place in the scoreboard
+			var shortName = this.scoreBoards[i].playerName;
+            if(shortName.length > 4){
+            shortName = shortName.substring(0,4);
+            }
+			$('#'+ 'player' + (i+1) ).html(shortName);
 		}
 
 	}
@@ -501,6 +507,16 @@ class Game{
 		    if(keyA < keyB) return 1;
 		    return 0;
 		});
+	}
+
+    addname(playerName, numOfPlayers){
+
+		//for (var i = 0; i < numOfPlayers.length; i++) {
+			$('#player1').html(playerName);
+		//}
+		     console.log(playerName);
+
+
 	}	
 
 }
